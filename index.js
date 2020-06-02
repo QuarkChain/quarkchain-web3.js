33198f6db5ae06173ced4705e6cf67a8191c75d2
superman285 <superman285@126.com>
2020-06-02 14:37:42 +0800

    combine condition

--- index.js
+++ index.js
@@ -35,9 +35,7 @@ import Transaction from './quarkchain-ethereum-tx';
 
 let Web3;
 /* eslint-disable */
-if (typeof window === 'undefined') {
-  Web3 = require('web3');
-} else if (!window.Web3 || (window.Web3.version && !window.Web3.version.api)) {
+if (typeof window === 'undefined' || !window.Web3 || (window.Web3.version && !window.Web3.version.api)) {
   //no Web3 or window.Web3 version 1.x
   Web3 = require('web3');
 } else {
